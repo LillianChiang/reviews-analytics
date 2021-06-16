31ac3af8f2883a3cd5c33a1f0990bf5b591c638a
LillianChiang <45815283+LillianChiang@users.noreply.github.com>
2021-06-16 11:01:13 +1000

    Add files via upload

--- record.py
+++ record.py
@@ -6,11 +6,33 @@ with open('reviews.txt','r') as f:
         count += 1 # count = count + 1
         if count % 100000 == 0:
             print (len(data))
+print('There are total:', len(data), 'records')
 
-print('there are total:', len(data), 'records')
 
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
-
 print('The average length of records is:', sum_len/len(data))
+
+
+new  = []
+for d in data:
+        if len(d) < 100:
+            new.append(d)
+print('There are ', len(new), 'records less than 100 words')
+print('--------------------------------------------------------------------------')
+print(new[0])
+print('--------------------------------------------------------------------------')
+print(new[1])
+
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('There are ',len(good), 'records mentioned good')
+print(good[0])
+
+
+
+
